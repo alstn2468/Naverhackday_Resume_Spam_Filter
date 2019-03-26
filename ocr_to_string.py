96a11d72aad13792203dba0df4d0cd8c885fa731
alstn2468 <alstn2468_@naver.com>
2019-03-27 00:05:12 +0900

    Add ocr_to_str function, str_to_text function, Continue testing...

--- ocr_to_string.py
+++ ocr_to_string.py
@@ -3,8 +3,51 @@ from pytesseract import *
 import configparser
 import os
 
-# Config parser Init
+# Config parser 초기화
 config = configparser.ConfigParser()
-# Config File Read
+# Config File 읽기
 config.read(os.path.dirname(os.path.realpath(__file__))
             + os.sep + 'envs' + os.sep + 'property.ini')
+
+def ocr_to_str(fullPath, outTxtPath, fileName, lang="eng"):
+    # Image path
+    image_path = Image.open(fullPath)
+    textName = os.path.join(outTxtPath, fileName.split('.')[0])
+
+    '''추출(이미지파일, 추출언어, 옵션)
+    preserve_interword_spaces : 단어 간격 옵션 조절
+    psm(페이지 세그먼트 모드) : 이미지 영역안에서 텍스트 추출 범위 모드
+    '''
+    outputText = image_to_string(
+        image_path, lang=lang, config='--psm 1 -c preserve_interword_spaces=1'
+    )
+
+
+    print("+++ OCT Extract Result +++")
+    print("Extract File Name :", fileName)
+    print("\n")
+
+    print(outputText)
+
+    str_to_text(textName, outputText)
+
+def str_to_text(textName, outputText):
+    with open(textName + ".txt", "w", encoding="utf-8") as f:
+        f.write(outputText)
+
+if __name__ == "__main__":
+
+    outTxtPath = os.path.dirname(os.path.realpath(__file__)
+                                 + config["Path"]["OcrTxtPath"])
+
+    print(outTxtPath)
+
+    for root, dirs, files in os.walk(
+        os.path.dirname(os.path.realpath(__file__)) + config["Path"]["OriImgPath"]):
+
+        for fileName in files:
+            fullName = os.path.join(root, fileName)
+
+            print(fileName, fullName)
+
+            ocr_to_str(fullName, outTxtPath, fileName, "kor+eng")
